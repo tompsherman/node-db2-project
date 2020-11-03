0455f01c548ca7e31288f15a727a9465e21aef97
unknown <tompsherman@gmail.com>
2020-11-03 15:57:53 -0500

    now we're cookin with gas

--- router/router.js
+++ router/router.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const db = require('../data/config')
 
 router.get('/testing', (req,res)=>{ 
-    user.get()
+    db('cars')
     .then(data => res.status(200).json({message: "hello chap"}))
     .catch(error => res.status(500).json({message: `${error.message}; ${error.stack}`}))
  })
